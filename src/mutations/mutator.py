89efe5a9bbf376c373aa5ef9a3ab331001a16ff5
Camilo Perez <ca.perez17@uniandes.edu.co>
2025-12-09 20:18:58 -0500

    Add mutator.py module for protein mutations

---
+++ src/mutations/mutator.py
@@ -0,0 +1,31 @@
+"""
+Protein mutation utilities.
+"""
+
+def generate_mutation(sequence: str, position: int, new_aa: str):
+    """
+    Generates a point mutation in a protein sequence.
+    
+    Args:
+        sequence: Original amino acid sequence
+        position: Position to mutate (0-indexed)
+        new_aa: New amino acid to insert
+        
+    Returns:
+        TODO: Implement
+    """
+    pass
+
+
+def validate_amino_acid(aa: str):
+    """
+    Validates if a character is a valid amino acid.
+    
+    Args:
+        aa: Single character to validate
+        
+    Returns:
+        TODO: Implement
+    """
+    pass
+
